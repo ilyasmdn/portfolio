1a8971bdfbfa11e8ef37a22de26fcaadc42f1c7e
Ivan <ilyaselmadni1@gmail.com>
2025-03-28 18:33:48 +0000

    create contact section

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import Hero from "./components/Hero";
 import About from "./components/About";
 import { useEffect, useState } from "react";
 import Projects from "./components/Projects";
+import Contact from "./components/Contact";
 
 
 type Theme = 'light' | 'dark';
@@ -32,6 +33,7 @@ const App = () => {
         <Hero />
         <About />
         <Projects />
+        <Contact />
       </main>
     </>
   );
---
+++ src/components/Contact.tsx
@@ -0,0 +1,12 @@
+
+const Contact = () => {
+  return (
+    <section id="contact" className="flex flex-col items-center gap-16 py-20">
+        <h2 className="section-title">Contact</h2>
+      <div className="section-container flex flex-col items-left w-full">
+      </div>
+    </section>
+  )
+}
+
+export default Contact
