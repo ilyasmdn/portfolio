660b409c6e37b481be2aa5c7931ced6068055bfa
Ivan <ilyaselmadni1@gmail.com>
2025-03-29 15:52:10 +0000

    small tweak

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -11,8 +11,8 @@ const Projects = () => {
     <section id="projects" className="flex flex-col items-center gap-16 py-20">
       <h2 className="section-title">{projectsContent.title}</h2>
       <div className="section-container grid grid-cols-[repeat(auto-fill,minmax(350px,1fr))] gap-4 w-full">
-        {projectsContent.list.map((project) => {
-          return <ProjectFile key={project.name} project={project} />;
+        {projectsContent.list.map((project, index) => {
+          return <ProjectFile key={index} project={project} />;
         })}
       </div>
     </section>
