de778b97fd98df0b009d11b45cf3060784567bf0
Ivan <ilyaselmadni1@gmail.com>
2025-05-02 17:06:20 +0100

    Add @vercel/speed-insights package and integrate SpeedInsights component

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,14 @@ import "./index.css";
 import App from "./App.tsx";
 import { LanguageProvider } from "./languages/LangugeContext.tsx";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react"
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <LanguageProvider>
       <App />
       <Analytics />
+      <SpeedInsights />
     </LanguageProvider>
   </StrictMode>
 );
