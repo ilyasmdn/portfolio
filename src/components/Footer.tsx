e068ad3c19327fda818faf8c94de1a882a0b9261
Ivan <ilyaselmadni1@gmail.com>
2025-05-02 15:35:20 +0100

    Add target="_blank" to LinkedIn link in Footer component

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -33,7 +33,7 @@ const Footer = () => {
             <a href="https://github.com/ilyasmdn/" target="_blank" aria-label="github link" className="hover:text-accent transition">
               <FontAwesomeIcon icon={faGithub} className="text-xl" />
             </a>
-            <a href="https://www.linkedin.com/in/ilyas-elmadni/" aria-label="linkedin link" className="hover:text-accent transition">
+            <a href="https://www.linkedin.com/in/ilyas-elmadni/" target="_blank" aria-label="linkedin link" className="hover:text-accent transition">
               <FontAwesomeIcon icon={faLinkedin} className="text-xl" />
             </a>
             <a href="#footer" aria-label="twitter link" className="hover:text-accent transition">
